d8342a804f78662d33d8b22f317fc24ac8e11c7b
Miguel Angel Vigo Baz <vigobaz@hotmail.com>
2024-11-19 21:48:07 +0100

    Añadido Mensaje de Bienvenida

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,5 +1,6 @@
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("¡Hola, mundo desde IntelliJ IDEA!");
+        System.out.println("Bienvenidos al curso de DAW");
     }
 }
